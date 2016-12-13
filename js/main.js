a25be663af2f3be002cf3dd6de6e26da1be8c29c
sidhemu <sidhemu2017@gmail.com>
2016-12-13 17:57:15 +0530

    latest commit

--- js/main.js
+++ js/main.js
@@ -5,13 +5,12 @@ app.controller('musicController', function($scope, $http) {
       fetch();
     });
 
-    $scope.search = "http://104.197.128.152:8000/v1/tracks?page=1";
+    $scope.search = "http://104.197.128.152:8000/v1/tracks?page=2";
     function fetch() {
           $http.get($scope.search)
             .then(function(response) {
+              $scope.page = response.data;
               $scope.details = response.data.results;
-              $scope.songType =  $scope.details.genres;
-              console.log($scope.details);
             });
 
         }
