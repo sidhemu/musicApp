996ca4efc81aef60be7a21c6d3ae3359b485749c
sidhemu <sidhemu2017@gmail.com>
2016-12-13 14:06:04 +0530

    pushing project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var app = angular.module('musicApp', []);
+
+app.controller('musicController', function($scope, $http) {
+    $scope.$watch('search', function() {
+      fetch();
+    });
+
+    $scope.search = "http://104.197.128.152:8000/v1/tracks?page=1";
+    function fetch() {
+          $http.get($scope.search)
+            .then(function(response) {
+              $scope.details = response.data;
+            });
+
+        }
+    });
