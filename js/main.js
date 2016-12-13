a2a0076b696af66869e38b8b18810247507e7344
sidhemu <sidhemu2017@gmail.com>
2016-12-13 18:31:31 +0530

    latest commit for pagination

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ app.controller('musicController', function($scope, $http) {
       fetch();
     });
 
-    $scope.search = "http://104.197.128.152:8000/v1/tracks?page=2";
+    $scope.search = "http://104.197.128.152:8000/v1/tracks";
     function fetch() {
           $http.get($scope.search)
             .then(function(response) {
@@ -14,4 +14,21 @@ app.controller('musicController', function($scope, $http) {
             });
 
         }
+
+    $scope.previous = function(page){
+      $http.get(page)
+        .then(function(response) {
+          $scope.page = response.data;
+          $scope.details = response.data.results;
+        });
+    }
+
+    $scope.next = function(page){
+      $http.get(page)
+        .then(function(response) {
+          $scope.page = response.data;
+          $scope.details = response.data.results;
+        });
+    }
+
     });
